a76429a92ba820fd04d8972aaa7615203b1b4030
andrasatriapratama <andrasatriapratama@gmail.com>
2022-10-28 22:42:41 +0700

    list user

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: depend_on_referenced_packages
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_crud/model/user.dart';
 import 'package:firebase_crud/user_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -39,6 +41,21 @@ class _MainPageState extends State<MainPage> {
         appBar: AppBar(
           title: const Text('All Users'),
         ),
+        body: StreamBuilder<List<User>>(
+          stream: readUsers(),
+          builder: (context, snapsot) {
+            if (snapsot.hasError) {
+              return Text('Something wnt wrong! ${snapsot.error}');
+            } else if (snapsot.hasData) {
+              final users = snapsot.data!;
+              return ListView(
+                children: users.map(buildUser).toList(),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             Navigator.of(context).push(
@@ -50,4 +67,18 @@ class _MainPageState extends State<MainPage> {
           child: const Icon(Icons.add),
         ),
       );
+
+  Widget buildUser(User user) => ListTile(
+        leading: CircleAvatar(
+          child: Text('${user.age}'),
+        ),
+        title: Text(user.name),
+        subtitle: Text(user.birthday.toIso8601String()),
+      );
+
+  Stream<List<User>> readUsers() => FirebaseFirestore.instance
+      .collection('users')
+      .snapshots()
+      .map((snapsot) =>
+          snapsot.docs.map((doc) => User.fromJson(doc.data())).toList());
 }
--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
   String id;
@@ -7,14 +8,21 @@ class User {
 
   User(
       {this.id = '',
-        required this.name,
-        required this.age,
-        required this.birthday});
+      required this.name,
+      required this.age,
+      required this.birthday});
 
   Map<String, dynamic> toJson() => {
-    'id':id,
-    'name':name,
-    'age':age,
-    'birthday':birthday,
-  };
+        'id': id,
+        'name': name,
+        'age': age,
+        'birthday': birthday,
+      };
+
+  static User fromJson(Map<String, dynamic> json) => User(
+        id: json['id'],
+        name: json['name'],
+        age: json['age'],
+        birthday: (json['birthday'] as Timestamp).toDate(),
+      );
 }
