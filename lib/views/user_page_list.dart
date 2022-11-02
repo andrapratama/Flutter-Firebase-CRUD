4acd9c9123cd01ac5ef2f6de9beaa57738f6315e
andrasatriapratama <andrasatriapratama@gmail.com>
2022-11-02 21:09:40 +0700

    add delete dialog

--- lib/views/user_page_list.dart
+++ lib/views/user_page_list.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_crud/models/user.dart';
 import 'package:firebase_crud/views/user_page_add.dart';
@@ -12,68 +14,98 @@ class UserPageList extends StatefulWidget {
 }
 
 class _UserPageListState extends State<UserPageList> {
-
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text('All Users'),
-    ),
-    body: StreamBuilder<List<User>>(
-      stream: readUsers(),
-      builder: (context, snapsot) {
-        if (snapsot.hasError) {
-          return Text('Something went wrong! ${snapsot.error}');
-        } else if (snapsot.hasData) {
-          final users = snapsot.data!;
-          return ListView(
-            children: users.map(buildUser).toList(),
-          );
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const UserPage(),
-          ),
-        );
-      },
-      child: const Icon(Icons.add),
-    ),
-  );
+        appBar: AppBar(
+          title: const Text('All Users'),
+        ),
+        body: StreamBuilder<List<User>>(
+          stream: readUsers(),
+          builder: (context, snapsot) {
+            if (snapsot.hasError) {
+              return Text('Something went wrong! ${snapsot.error}');
+            } else if (snapsot.hasData) {
+              final users = snapsot.data!;
+              return ListView(
+                children: users.map(buildUser).toList(),
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const UserPage(),
+              ),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+      );
 
   Widget buildUser(User user) => ListTile(
-    leading: CircleAvatar(
-      child: Text('${user.age}'),
-    ),
-    title: Text(user.name),
-    subtitle: Text(user.birthday.toIso8601String()),
-    trailing: FittedBox(
-      fit: BoxFit.fill,
-      child: Row(
-        children: [
-          IconButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => UserPageEdit(id: user.id, name: user.name, age: user.age, birthday: user.birthday.toIso8601String()),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.edit)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.delete)),
-        ],
-      ),
-    ),
-  );
+        leading: CircleAvatar(
+          child: Text('${user.age}'),
+        ),
+        title: Text(user.name),
+        subtitle: Text(user.birthday.toIso8601String()),
+        trailing: FittedBox(
+          fit: BoxFit.fill,
+          child: Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => UserPageEdit(
+                            id: user.id,
+                            name: user.name,
+                            age: user.age,
+                            birthday: user.birthday.toIso8601String()),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.edit)),
+              IconButton(
+                  onPressed: () {
+                    AlertDialog delete = AlertDialog(
+                      title: const Text("Information"),
+                      content: Container(
+                        height: 100,
+                        child: Column(
+                          children: [
+                            Text("Are you sure for deleting ${user.name}")
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                            onPressed: () {
+
+                            },
+                            child: const Text("Yes")),
+                        TextButton(
+                          child: const Text('No'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                    showDialog(context: context, builder: (context) => delete);
+                  },
+                  icon: const Icon(Icons.delete)),
+            ],
+          ),
+        ),
+      );
 
   Stream<List<User>> readUsers() => FirebaseFirestore.instance
       .collection('users')
       .snapshots()
       .map((snapsot) =>
-      snapsot.docs.map((doc) => User.fromJson(doc.data())).toList());
-
+          snapsot.docs.map((doc) => User.fromJson(doc.data())).toList());
 }
